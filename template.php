99d31a7e164953a5fb1986684ea9daa8534958a4
Samantha Shidler <samantha.shidler@gmail.com>
2019-02-05 22:02:46 -0800

    Add files via upload

---
+++ template.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="This will hold meta description content.">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale-1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Black|Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css" type="text/css">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <link rel="stylesheet/less"  href="css/styles.less" type="text/css" />
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js" ></script>
+    <title>Template-Ace in the Hole Multisport Events</title>
+</head>
+<header>
+    <?php include 'header.php'; ?>
+    <?php include 'navigation.php'; ?>
+</header>
+<body>
+    <div id="container">
+
+        <main>
+            <section>
+                <div class="section group">
+                    <div class="col span_1_of_2">
+                        <img src="#" alt="Images(Slideshow) go here.">
+                        <section>
+                        <h2>Heading 2</h2>
+                            This is column 1
+                        </section>
+                        <section>
+                            <h2>Heading 2</h2>
+                            This is column 1
+                        </section>
+                    </div>
+                    <div class="col span_1_of_2">
+                        <aside>
+                            <h3>Heading 3</h3>
+                            This is column 2
+                        </aside>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/scripts.js"></script>
+        <footer>
+            <?php include 'footer.php'; ?>
+        </footer>
+    </div>
+
+</body>
+</html>
